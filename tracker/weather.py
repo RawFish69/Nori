761b422616ff6905a627e32dbbb968fc8341f9ba
RawFish69 <108964215+RawFish69@users.noreply.github.com>
2023-01-29 14:43:49 -0600

    Update weather.py

--- tracker/weather.py
+++ tracker/weather.py
@@ -6,6 +6,9 @@ async def check_weather(ctx):
     city = ctx.options.location
     async with python_weather.Client(format=python_weather.IMPERIAL) as client:
         weather = await client.get(city)
+    hourly_data = []
+    the_dates = []
+    forecast_data = []
     temp_C = int((weather.current.temperature - 32) * 5 / 9)
     feels_like_F = weather.current.feels_like
     feels_like_C = int((feels_like_F - 32) * 5 / 9)
@@ -14,11 +17,22 @@ async def check_weather(ctx):
     display_weather += f'Region: {weather.nearest_area.region}, {weather.nearest_area.country}\n'
     display_weather += f"Current Temperature: {weather.current.temperature} F or {temp_C} C\n"
     display_weather += f'Feels like {feels_like_F} F or {feels_like_C} C\n'
-    display_weather += 'Upcoming Temperature\n'
     for forecast in weather.forecasts:
-        display_weather += (
-            f'{forecast.date} Highest Temp: {forecast.highest_temperature} F Lowest Temp: {forecast.lowest_temperature} F\n')
-    display_weather += f'Wind Direction: {weather.current.wind_direction}, Wind Speed: {weather.current.wind_speed} km/h\n'
+        the_dates.append(forecast.date)
+        hourly_data.clear()
+        for hourly in forecast.hourly:
+            hourly_data.append(f'[{hourly.time.hour}:{hourly.time.minute}{hourly.time.second}] {hourly.description}')
+        forecast_data.append(hourly_data)
+    index = 1
+    display_weather += f'{the_dates[0]} (Today):\n'
+    for var in forecast_data[0]:
+        if index < 2:
+            display_weather += f'{var} -> '
+            index += 1
+        else:
+            display_weather += f'{var}\n'
+            index = 1
+    display_weather += f'Wind Direction: {weather.current.wind_direction}, Wind Speed: {weather.current.wind_speed} mph\n'
     display_weather += f'Humidity: {weather.current.humidity} %\n'
     display_weather += '```'
     print(display_weather)
