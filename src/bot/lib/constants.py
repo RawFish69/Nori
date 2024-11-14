b4f808c318f27ada537f4edd752d1eac66fa4f5f
RawFish69 <bfzh233@gmail.com>
2024-11-14 16:45:08 -0500

    bot library source code (partial)

---
+++ src/bot/lib/constants.py
@@ -0,0 +1,29 @@
+"""
+Author: RawFish
+Description: Constants and configuration values
+"""
+
+VERSION = "1.0.0"
+
+STAT_MAPPING = {
+    # load from stats_mapping.json
+    # there should be a similar mapping on Wynnbuilder repo, or maybe wynntils
+    # I will upload the custom one I use when it's fully open sourced
+}
+
+TIER_COLORS = {
+    "mythic": "#AA00AA",
+    "fabled": "#FF5555",
+    "legendary": "#55FFFF",
+    "rare": "#FF55FF",
+    "unique": "#c1c11f",
+    "colaon": "#FFFFFF"
+}
+
+TIER_TRANSLATIONS = {
+    "mythic": "Mythic",
+    "fabled": "Fabled",
+    "legendary": "Legendary",
+    "rare": "Rare",
+    "unique": "Unique",
+}
